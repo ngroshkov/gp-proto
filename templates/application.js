70727b9db237d5efc736bf3e80748419d0c7392f
ngroshkov <ngroshkov@yandex.ru>
2017-09-23 00:11:57 +0300

    load all resources in webpack

--- templates/application.js
+++ templates/application.js
@@ -1,11 +1,15 @@
 "use strict";
 
-import $ from 'jquery';
-import mapboxgl from 'mapbox-gl';
+const css = require("./style.css");
+
+const boundaryjson = require('json-loader!./data/cityboundary.geojson');
+
+const mapbox = require('mapbox-gl');
 const turf = require('@turf/turf');
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2xuNCIsImEiOiJjaW9sNjZlbWMwMDEwdzVtNmxxYjA2ZGozIn0.BytaphQwtjCVMGEaLlfb3Q';
-var map = new mapboxgl.Map({
+
+mapbox.accessToken = 'pk.eyJ1Ijoia2xuNCIsImEiOiJjaW9sNjZlbWMwMDEwdzVtNmxxYjA2ZGozIn0.BytaphQwtjCVMGEaLlfb3Q';
+var map = new mapbox.Map({
     container: 'map',
     style: 'mapbox://styles/kln4/cj7kgebwh00ol2rpq2b8h6udp',
     center: [45.502,41.925],
@@ -13,23 +17,21 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function () {
-    $.getJSON('/static/data/cityboundary.geojson', function(data) {
-        let boundary = data.features[0];
-        let bounds = turf.bboxPolygon([180, 90, -180, -90]); 
-        map.addSource('boundary-source', {
-            type: 'geojson',
-            data: turf.difference(bounds, boundary)
-        });
+    let boundary = boundaryjson.features[0];
+    let bounds = turf.bboxPolygon([180, 90, -180, -90]); 
+    map.addSource('boundary-source', {
+        type: 'geojson',
+        data: turf.difference(bounds, boundary)
+    });
         
-        map.addLayer({
-            "id": "boundary-layer",
-            "source": "boundary-source",
-            "type": "fill",
-            "paint": {
-                "fill-color": "grey",
-                "fill-opacity": 0.5,
-                "fill-outline-color": "red"
-            }
-        });
+    map.addLayer({
+        "id": "boundary-layer",
+        "source": "boundary-source",
+        "type": "fill",
+        "paint": {
+            "fill-color": "grey",
+            "fill-opacity": 0.5,
+            "fill-outline-color": "red"
+        }
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  module: {
+      rules: [{
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+      }],
+      loaders: [{
+          test: /\.(json|geojson)$/,
+          loader: 'json-loader'
+      }]
+    }
 };
