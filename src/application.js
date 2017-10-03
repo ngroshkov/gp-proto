e670019a012c9761326984b30defbfd1203aaa7d
ngroshkov <ngroshkov@yandex.ru>
2017-10-03 13:24:22 +0300

    style toolbar

--- src/application.js
+++ src/application.js
@@ -41,8 +41,8 @@ function GpMap(props) {
 
 class CityBoundaryLayer extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {boundary: {"type":"FeatureCollection", "features":[]}};
+	super(props);
+	this.state = {boundary: {"type":"FeatureCollection", "features":[]}};
   }
 
   componentDidMount() {
@@ -55,13 +55,13 @@ class CityBoundaryLayer extends React.Component {
   }
 
   setBoundary(boundary) {
-    this.setState({
-      boundary: boundary
-    });
+	this.setState({
+	  boundary: boundary
+	});
   }
   
   render() {
-    return <GeoJSONLayer
+	return <GeoJSONLayer
 		data={this.state.boundary}
 		fillPaint={{
 			"fill-color": "grey",
@@ -92,28 +92,52 @@ function Toolbar(props) {
 }
 
 function Title(props) {
-	return <div><h2><i>Prototype</i></h2></div>;
+	return <div><h2>Telavi</h2></div>;
 }
 
 function Image(props) {
 	const style = {
 		width: '100%',
-		height: '150px',
-		backgroundColor: 'lightgray'
+		height: '150px'
 	}
-	return <div style={style}/>;
+	return <img src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Telavi_-_old_city.jpg" style={style}/>;
 }
 
 function Text(props) {
-	const style = {
-		padding: '10px',
-		textAlign: 'left'
+		const conainerstyle = {
+		position: 'relative',
+		height: '280px'
 	}
-	return <div style={style}>&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-		nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
-		in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-		occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+	const textconainerstyle = {
+		height: '100%',
+		overflow: 'hidden',
+		boxSizing: 'border-box',
+		position: 'absolute'
+	}
+	const textstyle = {
+		textAlign: 'justify',
+		position: 'relative',
+		maxHeight: '100%',
+		overflow: 'auto'
+	}
+	return  <div style={conainerstyle}>
+				<div style={textconainerstyle}>
+					<div style={textstyle}>
+						<p>&nbsp;&nbsp;&nbsp;&nbsp;<b>Telavi</b> (Georgian: <span lang="ka">თელავი</span> [tʰɛlɑvi])
+						is the main city and administrative center of Georgia's eastern province of Kakheti.
+						Its population consists of some 19,629 inhabitants (as of the year 2014). 
+						The city is located on foot-hills of Tsiv-Gombori Range at 500–800 meters above the sea level.</p>
+						<p>The first archaeological findings from Telavi date back to the Bronze Age. 
+						One of the earliest surviving accounts of Telavi is from the 2nd century AD, 
+						by Greek geographer Claudius Ptolemaeus, who mentions the name <i>Teleda</i> (a reference to <i>Telavi</i>). 
+						Telavi began to transform into a fairly important and large political and administrative center 
+						in the 8th century AD. Interesting information on Telavi is provided in the records by an Arab geographer, 
+						Al-Muqaddasi of the 10th century, who mentions Telavi along with such important cities of that time's 
+						Caucasus as Tbilisi, Shamkhor, Ganja, Shemakha and Shirvan. Speaking about the population of Telavi, 
+						Al-Muqaddasi points out that for the most part it consisted of Christians.</p>
+				 	</div>
+				</div>
+			</div>;
 }
 
 
